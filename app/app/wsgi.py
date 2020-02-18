6523344b8b61269bb825ab6a308e4500a7de3e34
DanielTitkov <mytasmic@gmail.com>
2020-02-19 01:41:14 +0300

    please work

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
+print("BEFORE APP")
 application = get_wsgi_application()
+print("AFTER APP")
